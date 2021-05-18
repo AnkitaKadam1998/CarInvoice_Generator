1e8ccde9a24364fde7c43c1923be6b6f04f2be98
komal@komal405.com <ankita80kadam@gmail.com>
2021-05-18 16:42:53 +0530

    give distance and time the invoice generator should return total fare

--- CabService.java
+++ CabService.java
@@ -1,7 +1,20 @@
-class CabService
-{
-public static void main(String args[])
-{
-Sytsem.out.println("Welcome");
+package com.tdd_day30;
 
+public class CabService {
+
+	public double calculatefare(final double distance, final int minute) 
+	{
+	double totalfare = 0;
+	if(distance == 0.0 && minute == 0)
+	{
+		totalfare=0;
+	}
+	if(distance < 1.0 && minute <=5)
+	{
+		return totalfare;
+	}
+	totalfare=(distance * 10) + minute;
+	
+	return totalfare;
+	}
 }
