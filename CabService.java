16b6dcaa1680fdd89096435e8ae9ecb2899dc466
komal@komal405.com <ankita80kadam@gmail.com>
2021-05-23 12:21:29 +0530

    ADD:code return total number of rides,totalfare,average fare per ride

--- CabService.java
+++ CabService.java
@@ -1,26 +1,45 @@
 package com.tdd_day30;
 
 public class CabService {
+	
+		double totalfare=0;
+		double totalFareForMultipleRides;
+		double averagefarePerRide;
+		
+		public double calculateFare(final double distance, final int minute) {
+			
+			totalfare=(distance*10) + minute;
+			
+			if(totalfare==0.0) {
+				return totalfare=0;			
+			}
+				
+			if(totalfare<5.0 ) {
+				return totalfare=5;
+			}
+			
+			return totalfare;
+		}
 
-	public double calculatefare(final double distance, final int minute) 
-	{
-	double totalfare = 0;
-	if(distance == 0.0 && minute == 0)
-	{
-		totalfare=0;
-	}
-	if(distance < 1.0 && minute <=5)
-	{
-		return totalfare;
-	}
-	totalfare=(distance * 10) + minute;
-	
-	return totalfare;
-	}
-	
-	public double totalfareforMultipleride(final double totalfare2,final int numofride)
-	{
-		double totalfareforMultipleride=totalfare2* numofride;
-		return totalfareforMultipleride;
-	}
+		public double calculateFare(Ride[] rides) {
+			for(Ride ride:rides) {
+				totalfare+=this.calculateFare(ride.distance, ride.minute);
+			}
+			return totalfare;
+		}
+
+		public InvoiceSummary calculateTotalFare(Ride[] ride) {
+			double totalfare = 0;
+			double averageFare = 0;
+			for(Ride rides: ride) {
+				totalfare += this.calculateFare(rides.distance, rides.minute);	
+			}
+			 averageFare = totalfare/ride.length;
+			return new InvoiceSummary(ride.length, totalfare, averageFare);
+		}
+
+		public double totalfareforMultipleride(double totalfare2, int numofride) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
 }
