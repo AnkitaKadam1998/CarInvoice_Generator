2cba94cd4d15f660e90375167b4be843191e456b
komal@komal405.com <ankita80kadam@gmail.com>
2021-05-23 12:25:10 +0530

    ADD:code return total number of rides,totalfare,average fare per ride

---
+++ Ride.java
@@ -0,0 +1,10 @@
+package com.tdd_day30;
+
+public class Ride {
+		public double distance;
+		public int minute;
+		public Ride(double distance, int minute) {
+			this.distance=distance;
+			this.minute=minute;
+		}
+}
