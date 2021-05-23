da311cdb0fa6f7439ec56693cf52e1e7f0900dbd
komal@komal405.com <ankita80kadam@gmail.com>
2021-05-23 13:15:49 +0530

    ADD:code return total number of rides,totalfare,average fare per ride

--- CabServiceTest.java
+++ CabServiceTest.java
@@ -6,53 +6,52 @@ import org.junit.Test;
 
 public class CabServiceTest {
 	
-	@Test
-	public void testCalculatorFare_should_return_total_fare()
-		{
-			CabService cabservice = new CabService();
-			
-			final double distance=260;
-			final int minute=90;
-			double totalfare= cabservice.calculatefare(distance,minute);
-			
-			assertEquals(2690,totalfare,0);
-		}
-	
-	@Test
-	public void testCalculatorFare_should_return_min_fare()
-		{
-			CabService cabservice = new CabService();
-			
-			final double distance=0.5;
-			final int minute=5;
-			double totalfare= cabservice.calculatefare(distance,minute);
-			
-			assertEquals(5,totalfare,0);
-		}
-	
-	@Test
-	public void testCalculatorFare_should_return_zero_fare()
-		{
-			CabService cabservice = new CabService();
-			
-			final double distance=0.0;
-			final int minute=0;
-			double totalfare= cabservice.calculatefare(distance,minute);
-			
-			assertEquals(0,totalfare,0);
-		}
-	
-	@Test
-	public void testCalculatorFare_should_return_total_fare_multipleride()
-		{
-			CabService cabservice = new CabService();
-			
-			final double distance=2.3;
-			final int minute=7;
-			final int numofride=3;
-			double totalfare= cabservice.calculatefare(distance,minute);
-			double totalfareformultipleride=cabservice.totalfareforMultipleride(totalfare, numofride);
-			assertEquals(90,totalfare,0);
-		}
+CabService cabService = new CabService();
 
+@Test
+public void testCalculateFare_Should_Return_Total_Fare() {
+	
+	final double distance =260;
+	final int minute=90;
+	double totalFare=cabService.calculateFare(distance,minute);
+	assertEquals(2690, totalFare,0);
+}
+
+@Test
+public void testCalculateFare_Should_Return_Min_Fare() {
+	
+	final double distance =0.1;
+	final int minute=1;
+	double totalFare=cabService.calculateFare(distance,minute);
+	assertEquals(5, totalFare,0);
+}
+
+@Test
+public void testCalculateFare_Should_Return_Zero_Fare() {	
+	final double distance =0.0;
+	final int minute=0;
+	double totalFare=cabService.calculateFare(distance,minute);
+	assertEquals(0, totalFare,0);
+}
+
+@Test
+public void testCalculateTotalFare_Should_ReturnTotalFare_forMultipleRides() {	
+	
+	Ride [] rides= {new Ride(2.0,5),
+					new Ride(0.1,1)
+							};
+	double Fare=cabService.calculateFare(rides);
+	assertEquals(30.0, Fare , 0);
+}
+
+@Test
+public void test_shouldReturn_TotalFare_averageFarePerRide_and_numOfRides() {	
+	Ride [] rides= {new Ride(2.0,5),
+					new Ride(0.1,1)
+					};
+
+	InvoiceSummary invoiceSummary = cabService.calculateTotalFare(rides);
+	InvoiceSummary expected = new InvoiceSummary(2, 30, 15);
+	assertEquals(expected, invoiceSummary);	
+}	
 }
